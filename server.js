28a2c4142684a5a9f0e6eed2e998ce3457ddd0dc
Daniel Pereira <danielpaulpereira@protonmail.com>
2021-01-22 02:06:27 -0500

    Initial commit.

---
+++ server.js
@@ -0,0 +1,14 @@
+// Daniel Pereira 2021
+// WEB322 A1
+
+var HTTP_PORT = process.env.port || 8080;
+var express = require("express");
+var app = express();
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
